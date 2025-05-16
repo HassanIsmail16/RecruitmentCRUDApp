0a136f59df374dd7ad1fbc7ba84e29179e023777
Adham Hamdy <adhamhn333@gmail.com>
2025-05-16 04:35:15 +0300

    Added class models

---
+++ RecruitmentCRUDApp/Models/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Company
+    {
+        // primary key
+        public int companyID { get; set; }
+
+        // foreign key
+        public int? managerID { get; set; }
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public byte[] logo { get; set; }
+        public string description { get; set; }
+
+
+        public virtual Employer Manager { get; set; }
+        public virtual ICollection<Vacancy> Vacancies { get; set; } = new List<Vacancy>();
+        public virtual ICollection<Employer> Employees { get; set; } = new List<Employer>();
+    }
+}
---
+++ RecruitmentCRUDApp/Models/JobSeeker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class JobSeeker
+    {
+        // foreign key
+        public int userID { get; set; }
+
+        public string skills { get; set; }
+        public byte[] resume { get; set; }
+        public string experiences { get; set; }
+        public string preferredLocation { get; set; }
+        public string interests { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual ICollection<Application> Applications { get; set; } = new List<Application>();
+        public virtual ICollection<SavedJob> SavedJobs { get; set; } = new List<SavedJob>();
+    }
+}
