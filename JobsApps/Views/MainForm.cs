e22f74aef6ad7f7ab77aad0265ca74a5cfaadbda
Mry3zof <yousseffsayd@gmail.com>
2025-05-17 03:47:32 +0300

    more views

--- JobsApps/Views/MainForm.cs
+++ JobsApps/Views/MainForm.cs
@@ -3,6 +3,7 @@ using DAL.Repositories;
 using Models;
 using RecruitmentApplication.Controllers;
 using RecruitmentApplication.Core.Navigation;
+using RecruitmentApplication.Views.Auth;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,18 +19,45 @@ namespace RecruitmentApplication.Views
 {
     public partial class MainForm : Form
     {
-        private INavigation navigation;
-        private RecruitmentContext context;
+        private readonly INavigation navigation;
+        private readonly RecruitmentContext context;
+
         public MainForm()
         {
-            InitializeComponent();
-            navigation = new Navigation(centralPanel);
+            try
+            {
+                InitializeComponent();
 
-            context = new RecruitmentContext();
+                if (Session.CurrentUser == null)
+                {
+                    MessageBox.Show("No user is logged in. Please login first.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
 
-            RegisterViews();
+                context = new RecruitmentContext();
 
-            navigation.NavigateTo<JobsControl>();
+                navigation = new Navigation(centralPanel);
+
+                RegisterViews();
+
+                navigation.NavigateTo<JobsControl>();
+
+                this.FormClosing += MainForm_FormClosing;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing MainForm: {ex.Message}\n\nStack Trace: {ex.StackTrace}",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                Application.Exit();
+            }
         }
 
         public void RegisterViews()
@@ -48,7 +76,25 @@ namespace RecruitmentApplication.Views
 
         private void navSavedJobsBtn_Click(object sender, EventArgs e)
         {
+            navigation.NavigateTo<SavedJobsControl>();
+        }
 
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form != this)
+                {
+                    form.FormClosing -= Form_FormClosing;
+                    form.Close();
+                }
+            }
+        }
+
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
         }
     }
 }
