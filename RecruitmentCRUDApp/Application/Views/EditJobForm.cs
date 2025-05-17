55d75e295779af5db96b1a31db738f3734b5696d
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-05-17 07:12:26 +0300

    added edit job form

---
+++ RecruitmentCRUDApp/Application/Views/EditJobForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecruitmentApplication.Views
+{
+    public partial class EditJobForm : Form
+    {
+        private int jobId;
+        public EditJobForm(int jobId)
+        {
+            InitializeComponent();
+            this.jobId = jobId;
+        }
+    }
+}
