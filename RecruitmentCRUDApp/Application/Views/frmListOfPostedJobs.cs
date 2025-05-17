8be85144f4f506da43a4a4a08add326c758fe2cf
Adham Hamdy <adhamhn333@gmail.com>
2025-05-17 20:30:47 +0300

    Added List of Posted Jobs UI

---
+++ RecruitmentCRUDApp/Application/Views/frmListOfPostedJobs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecruitmentApplication.Views
+{
+    public partial class frmListOfPostedJobs : Form
+    {
+        public frmListOfPostedJobs()
+        {
+            InitializeComponent();
+            InitializeControlButtons();
+        }
+
+        private void InitializeControlButtons()
+        {
+            if (dataGridPostedJobs.Columns.Contains("colControl"))
+            {
+                dataGridPostedJobs.Columns.Remove("colControl");
+            }
+
+            var editButtonColumn = new DataGridViewButtonColumn();
+            editButtonColumn.Name = "colEdit";
+            editButtonColumn.HeaderText = "Edit";
+            editButtonColumn.Text = "Edit";
+            editButtonColumn.UseColumnTextForButtonValue = true;
+            dataGridPostedJobs.Columns.Add(editButtonColumn);
+
+            var deleteButtonColumn = new DataGridViewButtonColumn();
+            deleteButtonColumn.Name = "colDelete";
+            deleteButtonColumn.HeaderText = "Delete";
+            deleteButtonColumn.Text = "Delete";
+            deleteButtonColumn.UseColumnTextForButtonValue = true;
+            dataGridPostedJobs.Columns.Add(deleteButtonColumn);
+        }
+    }
+}
