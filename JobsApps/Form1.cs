ed26bab29768eff940469e12e76d2e05dad40d17
Eslam <eslamsaeed298@gmail.com>
2025-05-17 03:06:31 +0300

    sql connection

--- JobsApps/Form1.cs
+++ JobsApps/Form1.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace JobsApps
 {
     public partial class Form1 : Form
@@ -6,5 +17,12 @@ namespace JobsApps
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connStr = "Data Source=LAPTOP-7RP4DTQ6;Initial Catalog=jobs;Integrated Security=True;Trust Server Certificate=True";
+            SqlConnection conn = new SqlConnection(connStr);
+        }
     }
 }
+            
